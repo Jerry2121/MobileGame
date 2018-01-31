68aa38b8bf372b91c4d30ea4908ed9f50f96f5f1
Jeremiah Templeton <19.jeremiah.templeto@ksd.org>
2018-01-31 10:11:06 -0800

    MainMenu is complete for at least functionallity. -JT

---
+++ UnityProject/MobileGame/Assets/Scripts/MainMenu/MainMenuScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Lvl1");
+    }
+    public void Difficulty()
+    {
+        SceneManager.LoadScene("MainMenu#2");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
