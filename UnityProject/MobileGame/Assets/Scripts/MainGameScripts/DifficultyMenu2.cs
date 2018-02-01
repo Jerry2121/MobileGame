fbc20f6d42f96cc551ba7bd663a5a27e3af65c9b
Jeremiah Templeton <19.jeremiah.templeto@ksd.org>
2018-02-01 10:26:45 -0800

    PauseMenu Added along with fixed buttons with highlight colors - JT

---
+++ UnityProject/MobileGame/Assets/Scripts/MainGameScripts/DifficultyMenu2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class DifficultyMenu2 : MonoBehaviour {
+    public GameObject CurrentyDifficultyText;
+    
+	// Use this for initialization
+	void Start () {
+        
+    }
+    public void Beginner()
+    {
+        PlayerPrefs.SetInt("Difficulty", 1);
+    }
+    public void Normal()
+    {
+        PlayerPrefs.SetInt("Difficulty", 2);
+    }
+    public void Insane()
+    {
+        PlayerPrefs.SetInt("Difficulty", 3);
+    }
+    // Update is called once per frame
+    void Update () {
+        if (PlayerPrefs.GetInt("Difficulty") == 1)
+        {
+            CurrentyDifficultyText.GetComponent<Text>().text = "Current Difficulty: Beginner";
+        }
+        if (PlayerPrefs.GetInt("Difficulty") == 2)
+        {
+            CurrentyDifficultyText.GetComponent<Text>().text = "Current Difficulty: Normal";
+        }
+        if (PlayerPrefs.GetInt("Difficulty") == 3)
+        {
+            CurrentyDifficultyText.GetComponent<Text>().text = "Current Difficulty: Insane";
+        }
+    }
+}
---
+++ UnityProject/MobileGame/Assets/Scripts/MainGameScripts/pausemenu.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pausemenu : MonoBehaviour {
+    private bool paused;
+    public GameObject DifficultyCanvas;
+    // Use this for initialization
+    void Start()
+    {
+        paused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && paused == false)
+        {
+            paused = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && paused == true)
+        {
+            paused = false;
+        }
+        if (paused)
+        { 
+            Time.timeScale = 0;
+            GetComponent<Canvas>().enabled = true;
+        }
+        if(paused == false)
+        { 
+            Time.timeScale = 1;
+            GetComponent<Canvas>().enabled = false;
+            DifficultyCanvas.GetComponent<Canvas>().enabled = false;
+        }
+    }
+
+
+    public void Resume()
+    {
+        DifficultyCanvas.GetComponent<Canvas>().enabled = false;
+        GetComponent<Canvas>().enabled = false;
+        paused = false;
+        Time.timeScale = 1;
+    }
+    public void Difficulty()
+    {
+        DifficultyCanvas.GetComponent<Canvas>().enabled = true;
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1;
+    }
+    public void DifficultyHide()
+    {
+        DifficultyCanvas.GetComponent<Canvas>().enabled = false;
+    }
+}
