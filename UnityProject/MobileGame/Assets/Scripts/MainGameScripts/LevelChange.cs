67b14358c691fa30437207089ce87e9fe4d218b4
Jeremiah Templeton <19.jeremiah.templeto@ksd.org>
2018-02-14 09:38:07 -0800

    Fixed UI for Mobile and Added Level Advancement. - JT

---
+++ UnityProject/MobileGame/Assets/Scripts/MainGameScripts/LevelChange.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChange : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "LvlBeat")
+        {
+            PlayerPrefs.SetInt("Lvlnum", PlayerPrefs.GetInt("Lvlnum") + 1);
+            if (PlayerPrefs.GetInt("Lvlnum") == 1 && PlayerPrefs.GetInt("Difficulty") == 1)
+            {
+                SceneManager.LoadScene("Lvl1Beginner");
+            }
+            if (PlayerPrefs.GetInt("Lvlnum") == 1 && PlayerPrefs.GetInt("Difficulty") == 2)
+            {
+                SceneManager.LoadScene("Lvl1Normal");
+            }
+            if (PlayerPrefs.GetInt("Lvlnum") == 1 && PlayerPrefs.GetInt("Difficulty") == 3)
+            {
+                SceneManager.LoadScene("Lvl1 Insane");
+            }
+            if (PlayerPrefs.GetInt("Lvlnum") == 2 && PlayerPrefs.GetInt("Difficulty") == 1)
+            {
+                SceneManager.LoadScene("Lvl2");
+            }
+            if (PlayerPrefs.GetInt("Lvlnum") == 2 && PlayerPrefs.GetInt("Difficulty") == 2)
+            {
+                SceneManager.LoadScene("Lvl2 Normal");
+            }
+            if (PlayerPrefs.GetInt("Lvlnum") == 2 && PlayerPrefs.GetInt("Difficulty") == 3)
+            {
+                SceneManager.LoadScene("Lvl2 Insane");
+            }
+        }
+    }
+}
